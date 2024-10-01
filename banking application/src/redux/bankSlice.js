18ed5b0e3ebf9409c53a9f11f97a8a2752b0f697
koshelvadim <koshelvadim@mail.ru>
2024-10-01 14:23:32 +0300

    test

--- banking application/src/redux/bankSlice.js
+++ banking application/src/redux/bankSlice.js
@@ -1,16 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  cash: JSON.parse(localStorage.getItem("CASH") || "0"),
+};
+
+const saveToLocalStorage = (cash) => {
+  localStorage.setItem("CASH", JSON.stringify(cash));
+};
+
 const bankSlice = createSlice({
   name: "bank",
-  initialState: {
-    cash: 100,
-  },
+  initialState,
   reducers: {
     addCash: (state, action) => {
       state.cash = state.cash + Number(action.payload);
+      saveToLocalStorage(state.cash);
     },
     getCash: (state, action) => {
       state.cash = state.cash - Number(action.payload);
+      saveToLocalStorage(state.cash);
     },
   },
 });
--- banking application/src/redux/usersSlice.js
+++ banking application/src/redux/usersSlice.js
@@ -1,16 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+const initialState = {
+  users: JSON.parse(localStorage.getItem('USERS') || "[]")
+};
+
+const saveToLocalStorage = (users) => {
+  localStorage.setItem("USERS", JSON.stringify(users));
+}
+
 const usersSlice = createSlice({
   name: "users",
-  initialState: {
-    users: [],
-  },
+  initialState,
   reducers: {
     addUser: (state, action) => {
       state.users = [...state.users, action.payload];
+      saveToLocalStorage(state.users);
     },
     removeUser: (state, action) => {
       state.users = state.users.filter(user => user.id !== action.payload.id);
+      saveToLocalStorage(state.users);
     },
   },
 });
